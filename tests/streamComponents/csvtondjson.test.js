470ec9f5aa46633e0eba4efc1e74d8100ab54923
Junior Paula <luke.junnior@icloud.com>
2023-06-28 19:02:47 -0300

    Ensure that works with files that has breaklines in the begging in of the string

--- tests/streamComponents/csvtondjson.test.js
+++ tests/streamComponents/csvtondjson.test.js
@@ -40,5 +40,28 @@ describe("CSVToNDJSON test suit", () => {
         expect(JSON.parse(current)).toStrictEqual(JSON.parse(expected))
     })
 
-    it.todo("it should work with files that has breaklines in the begging in of the string")
+    it("it should work with files that has breaklines in the begging in of the string", () => {
+        const csvString = "\n\nid,name,address\n\n01,Jhon,address01\n02,Janne,street\n\n"
+        const csvToJSON = new CSVToNDJSON({
+            delimiter: ",",
+            headers: ["id", "name", "address"]
+        })
+
+        const expected = [
+            JSON.stringify({id: "01", name: "Jhon", address: "address01"}),
+            JSON.stringify({id: "02", name: "Janne", address: "street"}),
+        ]
+
+        const fn = jest.fn()
+        
+        csvToJSON.on("data", fn)
+        csvToJSON.write(csvString)
+        csvToJSON.end()
+
+        const [firstCall] = fn.mock.calls[0]
+        const [secondsCall] = fn.mock.calls[1]
+
+        expect(JSON.parse(firstCall)).toStrictEqual(JSON.parse(expected[0]))
+        expect(JSON.parse(secondsCall)).toStrictEqual(JSON.parse(expected[1]))
+    })
 })
