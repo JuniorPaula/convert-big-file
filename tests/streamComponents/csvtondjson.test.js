2010ae074652a86b0c549142b0101a9381a5b563
Junior Paula <luke.junnior@icloud.com>
2023-06-28 18:53:16 -0300

    Ensure when a given csv string it should return a ndjson string

---
+++ tests/streamComponents/csvtondjson.test.js
@@ -0,0 +1,26 @@
+import { expect, describe, it, jest } from '@jest/globals'
+import CSVToNDJSON from '../../src/streamComponents/csvtondjson.js'
+
+describe("CSVToNDJSON test suit", () => {
+    it("given a csv string it should return a ndjon string", () => {
+        const csvString = "id,name,address\n01,Jhon,address01\n"
+        const csvToJSON = new CSVToNDJSON({
+            delimiter: ",",
+            headers: ["id", "name", "address"]
+        })
+
+        const expected = JSON.stringify({id: "01", name: "Jhon", address: "address01"})
+        const fn = jest.fn()
+        
+        csvToJSON.on("data", fn)
+        csvToJSON.write(csvString)
+        csvToJSON.end()
+
+        const [current] = fn.mock.lastCall
+
+        expect(JSON.parse(current)).toStrictEqual(JSON.parse(expected))
+    })
+
+    it.todo("it should work with string that doesnt contains breaklines at the end")
+    it.todo("it should work with files that has breaklines in the begging in of the string")
+})
